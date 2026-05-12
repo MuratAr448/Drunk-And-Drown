c8cb7e55aaf528163f351203c2fa8ef17b34b6d9
MuratAr448 <144346044+MuratAr448@users.noreply.github.com>
2026-05-12 11:28:56 +0200

    Update Utielety.cs

--- Drunk And Drown/Assets/Dev/Murat/Scripts/Utielety.cs
+++ Drunk And Drown/Assets/Dev/Murat/Scripts/Utielety.cs
@@ -5,11 +5,17 @@ public class Utielety : MonoBehaviour
 {
     public void ToGame()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("Movement Scene");
     }
     public void ToStartScreen()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("StartScreen");
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
     public void UnPause()
     {
