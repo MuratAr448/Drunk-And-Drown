ee844d25136bf9b8320ac0d7025f4029b8d49d5c
JerrelMartha <jerrelmartha@gmail.com>
2026-05-19 14:24:49 +0200

    UI

--- Drunk And Drown/Assets/Dev/Jerrel/Scripts/UI/UIButtonUtils.cs
+++ Drunk And Drown/Assets/Dev/Jerrel/Scripts/UI/UIButtonUtils.cs
@@ -7,4 +7,15 @@ public class UIButtonUtils : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("You succefully quitted the game");
+    }
+
+    public void ToggleUI(GameObject UI)
+    {
+        UI.SetActive(!UI.activeSelf);
+    }
 }
