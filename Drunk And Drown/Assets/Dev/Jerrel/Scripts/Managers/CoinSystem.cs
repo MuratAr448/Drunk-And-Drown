51a914efb79cc321e9b8b737b409bb3eb7dc696f
JerrelMartha <jerrelmartha@gmail.com>
2026-05-20 16:12:56 +0200

    Coin System
    - Enemies give Coins on Death

---
+++ Drunk And Drown/Assets/Dev/Jerrel/Scripts/Managers/CoinSystem.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class CoinSystem : MonoBehaviour
+{
+    [SerializeField] private int _coins;
+    [SerializeField] private float _coinMultiplier = 1.0f;
+
+    public static CoinSystem Instance;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
+    public void AddCoins(int amount)
+    {
+        _coins += Mathf.RoundToInt(amount * _coinMultiplier);
+    }
+
+    /// <summary>
+    /// Base coin multiplier is 1x. To increase add to amount, to decrease make amount negative.
+    /// </summary>
+    /// <param name="amount"></param>
+    public void AddCoinMultiplier(float amount)
+    {
+        _coinMultiplier += amount;
+    }
+
+    public int GetCoinAmount()
+    {
+        return _coins;
+    }
+}
