2a1735c8ebec8d08744b08e575b908ebaf38453c
JerrelMartha <jerrelmartha@gmail.com>
2026-05-29 14:56:40 +0200

    Weapon Manager

---
+++ Drunk And Drown/Assets/Dev/Jerrel/Scripts/Managers/WeaponManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public static WeaponManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    [Serializable]
+    public struct Weapon
+    {
+        public string id;
+        public GameObject weaponObject;
+    }
+
+    public List<Weapon> allWeapons = new List<Weapon>();
+
+    public void AddWeapon(Weapon weaponData)
+    {
+        if (weaponData.weaponObject == null) return;
+        MainPlayer.AddWeaponToList(weaponData.weaponObject);
+    }
+
+    [ContextMenu("Give Parrotgun")]
+    public void GiveParrotgun()
+    {
+        if (allWeapons != null && allWeapons.Count > 0 && allWeapons[0].weaponObject != null)
+        {
+            MainPlayer.AddWeaponToList(allWeapons[0].weaponObject);
+        }
+    }
+}
