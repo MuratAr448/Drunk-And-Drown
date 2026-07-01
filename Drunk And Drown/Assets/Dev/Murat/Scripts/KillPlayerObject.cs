5a68b41caef08d4254f51c02d17ca1f6fa3f0d25
MuratAr448 <144346044+MuratAr448@users.noreply.github.com>
2026-07-01 15:34:43 +0200

    Update KillPlayerObject.cs

--- Drunk And Drown/Assets/Dev/Murat/Scripts/KillPlayerObject.cs
+++ Drunk And Drown/Assets/Dev/Murat/Scripts/KillPlayerObject.cs
@@ -2,11 +2,26 @@ using UnityEngine;
 
 public class KillPlayerObject : MonoBehaviour
 {
-    public void OnTriggerEnter(Collider other)
+    private float timeForDamage = 0;
+    private void Update()
     {
-        if(other.TryGetComponent(out IDamageable Damaged))
+        timeForDamage += Time.deltaTime;
+        if (timeForDamage > 1)
         {
-            Damaged.TakeDamage(Damaged.CurrentHealth);
+            lava();
+            timeForDamage = 0;
+        }
+    }
+    public void lava()
+    {
+        Collider[] colliders = Physics.OverlapBox(transform.position,new Vector3(transform.localScale.x*0.5f, transform.localScale.y, transform.localScale.z * 0.5f));
+
+        foreach (Collider col in colliders)
+        {
+            if (col.TryGetComponent(out IDamageable Damaged))
+            {
+                Damaged.TakeDamage(5);
+            }
         }
     }
 }
